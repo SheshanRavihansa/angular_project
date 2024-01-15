9d32397ddab34419fc7e177b697ad735f0e9921d
SheshanRavihansa <ravihansasheshan@gmail.com>
2024-01-15 12:42:55 +0530

    made product component

---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+
+  templateUrl: './products.component.html',
+  // templateUrl: '<h3>This is inline template</h3>',
+
+  styleUrl: './products.component.css'
+})
+export class ProductsComponent {
+
+}
