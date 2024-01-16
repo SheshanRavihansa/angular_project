87efb3c6bb847f8b994daefdcd8fc9fb5a5e31bd
SheshanRavihansa <ravihansasheshan@gmail.com>
2024-01-16 22:07:55 +0530

    interpolation

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,3 +1,4 @@
+import { ReturnStatement } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,8 +7,20 @@ import { Component } from '@angular/core';
   templateUrl: './products.component.html',
   // templateUrl: '<h3>This is inline template</h3>',
 
-  styleUrl: './products.component.css'
+  styleUrl: './products.component.css',
+  interpolation :['$','$']
 })
 export class ProductsComponent {
+
+  firstProductItemName = "White Basmathi Rice";
+  secondProductItemName = "wheat Flour";
+
+  public getPriceofRice() {
+    return 350;
+  }
+
+  public getQuantityofFlour(){
+    return 225;
+  }
 
 }
