86db90efa92e4a5fab0758a149a546ad45a5555c
SheshanRavihansa <ravihansasheshan@gmail.com>
2024-01-22 14:08:24 +0530

    Lifecycle Hooks

--- src/app/components/add-poduct/add-poduct.component.ts
+++ src/app/components/add-poduct/add-poduct.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AddPoductComponent {
 
+  ngOnInit(): void {
+    alert("ngOnInit Called");
+    console.log("triggered ngOnInit")
+  }
+
+  ngDoCheck() {
+    console.log("triggered ngDoCheck");
+  }
+
+  ngAfterContentInit() {
+    console.log("triggered ngAfterContentInit");
+  }
+
+  ngAfterContentChecked() {
+    console.log("triggered ngAfterContentChecked");
+  }
+
+  ngAfterViewInit() {
+    console.log("triggered ngAfterViewInit");
+  }
+
+  ngAfterViewChecked() {
+    console.log("triggered ngAfterViewChecked");
+  }
+
+  ngOnDestroy() {
+    alert("ngDestroy Called");
+    console.log("triggered ngOnDestroy");
+  }
+
 }
