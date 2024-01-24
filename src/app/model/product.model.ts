f0d02b01fa5a4dab58e231571fcd3fb3a1219ace
SheshanRavihansa <ravihansasheshan@gmail.com>
2024-01-24 14:53:02 +0530

    product model

---
+++ src/app/model/product.model.ts
@@ -0,0 +1,12 @@
+export interface Product {
+  id: number;
+  name: string;
+  description: string;
+  category: string;
+  expiredDate: string;
+  manufacturedDate: string;
+  batchNumber: string;
+  quantity: number;
+  unitPrice: number;
+  createdDate: string;
+}
