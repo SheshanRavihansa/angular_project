f1cdf344a2e2d6ef36846b32bc81bf2e98e27530
SheshanRavihansa <ravihansasheshan@gmail.com>
2024-01-27 11:40:00 +0530

    typo

--- src/app/components/edit-product/edit-product.component.ts
+++ src/app/components/edit-product/edit-product.component.ts
@@ -8,7 +8,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class EditProductComponent {
 
   @Input()
-  prductId!: number;
+  productId!: number;
 
   @Output()
   cancelEditEvent: EventEmitter<void> = new EventEmitter<void>();
